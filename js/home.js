8f765efd793e54af2d4ea10a2caaeb9990d0de1e
innseonn <innseonn@gmail.com>
2022-11-14 14:35:08 +0900

    Feat: 스크롤 시 필요한 기능 추가 - TOP 이동 버튼 추가 - 상단 네비게이션바 고정 - 상단 네비게이션바 애니메이션 추가 - 관련 스타일 수정

--- js/home.js
+++ js/home.js
@@ -1,20 +1,64 @@
-const navBtn = document.querySelector('.navBtn');
-const fullNav = document.querySelector('.fullNav');
-const navLliks = document.querySelectorAll('.links a');
+const aside = document.querySelector('aside');
+const topBtn = aside.querySelector('.topBtn');
+const nav = document.querySelector('.nav');
+const navBtn = nav.querySelector('.navBtn');
+const fullNav = nav.querySelector('.fullNav');
+const navLliks = nav.querySelectorAll('.links a');
 
-navBtn.addEventListener('click', () => {
-	fullNav.classList.toggle('show');
+
+/** 이벤트 리스너 등록 **/
+topBtn.addEventListener('click', goToScroll);
+navLliks.forEach(v => v.addEventListener('click', goToScroll));
+navBtn.addEventListener('click', () => fullNav.classList.toggle('show'));
+
+/** 윈도우 스크롤 이벤트 리스너 **/
+let prevScroll = 0;
+window.addEventListener('scroll', () => {
+	let currScroll = window.scrollY;
+	if(prevScroll > currScroll) { //위로 스크롤
+		if(currScroll < 100) {
+			nav.style.animationName = 'slideUp';
+			nav.classList.remove('fixed');
+			aside.classList.remove('show');
+		}
+		else {
+			nav.style.animationName = 'slideDown';
+			nav.classList.add('fixed');
+			aside.classList.add('show');
+		}
+	}
+	else { //아래로 스크롤
+		nav.style.animationName = 'slideUp';
+		aside.classList.remove('show');
+	}
+	prevScroll = currScroll;
 });
 
-navLliks.forEach(v => v.addEventListener('click', (e) => goToScroll(e)));
-const goToScroll = (e) => {
+let timer = null;
+/** 타겟의 위치로 페이지를 스크롤하는 이벤트 리스너 함수 */
+function goToScroll(e) {
 	e.preventDefault();
-	const target = document.querySelector(e.target.getAttribute('href'));
-	const timer = setInterval(() => {
-		if(window.scrollY < target.offsetTop) {
-			window.scrollTo(0, window.scrollY + (target.offsetTop / 100));
-		} else {
-			clearTimeout(timer);
-		}
-	}, 0);
+
+	const target = document.querySelector(this.getAttribute('href'));
+	const top = window.scrollY - target.offsetTop > 0 ? true : false;
+
+	if(!timer) {
+		timer = setInterval(() => {
+			const dist = Math.abs(window.scrollY - target.offsetTop);
+			if(top) { //위로 스크롤
+				window.scrollTo(0, window.scrollY - Math.ceil(dist / 100));
+				if(window.scrollY <= target.offsetTop) {
+					clearInterval(timer);
+					timer = null;
+				}
+			}
+			else { //아래로 스크롤
+				window.scrollTo(0, window.scrollY + Math.ceil(dist / 100));
+				if(window.scrollY >= target.offsetTop) {
+					clearInterval(timer);
+					timer = null;
+				}
+			}
+		}, 0);
+	}
 }
